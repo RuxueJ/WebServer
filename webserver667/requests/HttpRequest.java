9d1135306dd795894a15dcf5d20a1c5ee389b6ef
guxu11 <guxu19970823@126.com>
2024-02-13 09:54:27 -0800

    add http method processors

--- webserver667/requests/HttpRequest.java
+++ webserver667/requests/HttpRequest.java
@@ -13,7 +13,7 @@ public class HttpRequest {
     private final Map<String, String> header;
     private MimeTypes mimeType;
 
-  private byte[] body ;
+    private byte[] body;
 
 
     public HttpRequest() {
@@ -25,98 +25,99 @@ public class HttpRequest {
         this.body = new byte[0];
         this.mimeType = null;
 
-  }
-
-  public HttpRequest(String uri, HttpMethods method, String queryString, String version, Map<String, String> header, byte[] body) {
-    this.uri = uri;
-    this.method = method;
-    this.queryString = queryString;
-    this.version = version;
-    this.header = header;
-    this.body = body;
-  }
-
-  public HttpMethods getHttpMethod() {
-
-    return this.method;
-  }
-
-  public void setHttpMethod(HttpMethods method) {
-    this.method = method;
-
-  }
-
-  public String getUri() {
-     if (this.uri.length() == 0){
-       return null;
-     }
-      int index = this.uri.indexOf("?");
-      if (index != -1){
-        return uri.substring(0,index);
-      }else{
-        return this.uri;
-      }
-
-  }
-
-  public void setUri(String uri) {
-    this.uri = uri;
-
-  }
-
-  public String getQueryString() {
-    if (this.uri.length() == 0){
-      return null;
-    }
-    int index = this.uri.indexOf("?");
-    if (index == -1){
-      return null;
-    }else{
-      return this.uri.substring(index+1);
     }
 
-  }
+    public HttpRequest(String uri, HttpMethods method, String queryString, String version, Map<String, String> header, byte[] body) {
+        this.uri = uri;
+        this.method = method;
+        this.queryString = queryString;
+        this.version = version;
+        this.header = header;
+        this.body = body;
+    }
 
-  public void setQueryString(String queryString) {
-    this.queryString = queryString;
+    public HttpMethods getHttpMethod() {
 
-  }
+        return this.method;
+    }
 
-  public String getVersion() {
-    return this.version;
-  }
+    public void setHttpMethod(HttpMethods method) {
+        this.method = method;
 
-  public void setVersion(String version) {
-    this.version = version;
+    }
 
-  }
+    public String getUri() {
+        if (this.uri.length() == 0) {
+            return null;
+        }
+        int index = this.uri.indexOf("?");
+        if (index != -1) {
+            return uri.substring(0, index);
+        } else {
+            return this.uri;
+        }
 
-  public String getHeader(String expectedHeaderName) {
-    return this.header.get(expectedHeaderName).strip();
-  }
+    }
 
-  public void addHeader(String headerLine) {
-    String key = headerLine.split(":")[0];
-    String value = headerLine.split(":")[1];
-    this.header.put(key,value);
+    public void setUri(String uri) {
+        this.uri = uri;
 
-  }
+    }
 
-  public int getContentLength() {
-    return Integer.parseInt(this.header.getOrDefault("Content-Length","0").strip());
-  }
+    public String getQueryString() {
+        if (this.uri.length() == 0) {
+            return null;
+        }
+        int index = this.uri.indexOf("?");
+        if (index == -1) {
+            return null;
+        } else {
+            return this.uri.substring(index + 1);
+        }
 
-  public byte[] getBody() {
-    return this.body;
-  }
+    }
 
-  public void setBody(byte[] body) {
-    this.body = body;
-  }
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
 
-  public boolean hasBody() {
-    return this.body.length > 0;
-  }
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+
+    }
+
+    public String getHeader(String expectedHeaderName) {
+        String headerContent = this.header.get(expectedHeaderName);
+        return headerContent == null ? null : headerContent.strip();
+    }
+
+    public void addHeader(String headerLine) {
+        String key = headerLine.split(":")[0];
+        String value = headerLine.split(":")[1];
+        this.header.put(key, value);
+
+    }
+
+    public int getContentLength() {
+        return Integer.parseInt(this.header.getOrDefault("Content-Length", "0").strip());
+    }
+
+    public byte[] getBody() {
+        return this.body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public boolean hasBody() {
+        return this.body.length > 0;
+    }
 
 
     public MimeTypes getMimeType() {
